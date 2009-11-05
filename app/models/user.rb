3cac4e9aadd5cd9f008d0090808d912db3d6baee
Jan Krutisch <jan@krutisch.de>
2009-11-05 18:19:35 +0100

    User fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     FeedbackRating.all(
       :joins => "JOIN feedbacks ON feedbacks.id = feedback_ratings.feedback_id JOIN users ON feedbacks.user_id = users.id",
       :group => "users.id", 
-      :select => "*,sum(feedback_ratings.rating) AS sum",
+      :select => "*,sum(feedback_ratings.rating) AS sum, sum(feedback_ratings.id) AS foobar",
       :order => 'sum DESC',
       :limit => options[:limit] || 10
     ).map{|fr| [fr.feedback.user, fr.sum] }
