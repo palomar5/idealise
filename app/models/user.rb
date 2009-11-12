29e1c4135ef60de008cf07a3726abadc38908c61
Jan Krutisch <jan@krutisch.de>
2009-11-12 15:53:11 +0100

    project notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ActiveRecord::Base
   has_many :projects
   has_many :feedbacks
   has_many :projects_with_feedback, :through => :feedbacks, :source => :project, :uniq => true
+  has_many :connected_users, :through => :project_feedbacks, :source => :user, :uniq => true
+  
+  has_many :project_feedbacks, :through => :projects, :source => :feedbacks, :uniq => true
+
+  
   
 end
