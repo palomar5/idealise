3cecc46106ae94a60615d02ad886c22f61559c4a
Wolfgang Wopperer <wolfgang.wopperer@mindmatters.de>
2009-11-13 13:30:39 +0100

    cross-linked tags

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,8 +13,15 @@ class ProjectsController < ResourceController::Base
     else
       @first_level_feedbacks = object.feedbacks.root
     end
+    @matching_users = @project.tag_list.collect { |tag| User.find_tagged_with(tag) }.flatten.uniq
     object.add_visit(current_user)
     true
+  end
+  
+  index.before do
+    unless params[:tag].blank?
+      @projects = Project.find_tagged_with(params[:tag])
+    end
   end
   
 end
