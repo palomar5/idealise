96dbbaaee854997285be7d8de9ec9eb0dca5c4fc
Wolfgang Wopperer <wolfgang.wopperer@mindmatters.de>
2009-11-16 17:17:31 +0100

    feedback rin

--- app/controllers/feedback_ratings_controller.rb
+++ app/controllers/feedback_ratings_controller.rb
@@ -16,7 +16,7 @@ class FeedbackRatingsController < ResourceController::Base
       format.html { redirect_to project_url(parent_object.project) }
       format.json { 
         render :json => { 
-          :toReplace => { "feedback_#{parent_object.id}_kudos" => kudos_sparkline_tag(parent_object.reload.kudos_history, "dd6644", "ffffff") } 
+          :toReplace => { "feedback_#{parent_object.id}_kudos" => kudos_sparkline_tag(parent_object.reload.kudos_history, "8c8c8c", "ffffff") } 
         }.to_json
       }
     end
